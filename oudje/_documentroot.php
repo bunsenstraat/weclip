c1b60675ba3fa1fd197cb1e1c826a7c4d0789fbe
vhost0062999 <vhost0062999@ssh.pcextreme.nl>
2019-12-29 10:33:24 +0100

    rm old stuff

--- oudje/_documentroot.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-	// DOCUMENT ROOT
-	$_SERVER['DOCUMENT_ROOT'] = '/home/vhosting/y/vhost0062999/domains/weclip.nl/htdocs/www';
--- oudje/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Show all information, defaults to INFO_ALL
-die($_SERVER['DOCUMENT_ROOT']);
-
-?>
